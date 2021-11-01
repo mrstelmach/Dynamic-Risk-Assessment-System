4e67eaf8bb96ba5a71364cd15d1cbf13f17d764d
mrstelmach <marekstelmach89@gmail.com>
2021-11-01 17:17:40 +0100

    Add get_vars_from_pandas function

--- training.py
+++ training.py
@@ -8,24 +8,25 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
-###################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+def get_vars_from_pandas(pandas_df, target_col='exited',
+                         predictors_list=['lastmonth_activity',
+                                          'lastyear_activity',
+                                          'number_of_employees']):
+    """Get X and y from provided pandas data frame."""
+    X, y = (pandas_df[predictors_list].to_numpy(),
+            pandas_df[target_col].to_numpy())
+    return X, y
 
 
-#################Function for training the model
-def train_model(pandas_df, target_col, variables, model_dir):
+def train_model(pandas_df, model_dir):
     """
-    Script for training logistic regression model on target_col target column
-    and predictors specified in variables list.
+    Script for training logistic regression model and saving to model_dir.
     """
     
-    X, y = pandas_df[variables].to_numpy(), pandas_df[target_col].to_numpy()
+    X, y = get_vars_from_pandas(pandas_df)
     
-    #use this logistic regression for training
+    # use logistic regression for training
     lr_model = LogisticRegression(
         C=1.0, class_weight=None, dual=False, fit_intercept=True,
         intercept_scaling=1, l1_ratio=None, max_iter=100,
@@ -34,15 +35,19 @@ def train_model(pandas_df, target_col, variables, model_dir):
         warm_start=False
     )
     
-    #fit the logistic regression to your data
+    # fit the logistic regression to data
     lr_model.fit(X, y)
     
-    #write the trained model to your workspace in a file called trainedmodel.pkl
+    # write the trained model to a file called trainedmodel.pkl
     with open(os.path.join(model_dir, 'trainedmodel.pkl'), 'wb') as model_file:
         pickle.dump(lr_model, model_file)
 
 
 if __name__ == "__main__":
+    with open('config.json', 'r') as f:
+        config = json.load(f) 
+    dataset_csv_path = os.path.join(config['output_folder_path']) 
+    model_path = os.path.join(config['output_model_path']) 
+    
     df = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
-    vars = ['lastmonth_activity', 'lastyear_activity', 'number_of_employees']
-    train_model(df, 'exited', vars, model_path)
+    train_model(df, model_path)
