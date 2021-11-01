d874b139c4ec35ca88a1a5977a5a4419ffd8a35b
mrstelmach <marekstelmach89@gmail.com>
2021-11-01 16:41:26 +0100

    Add function for model training

--- training.py
+++ training.py
@@ -17,16 +17,32 @@ model_path = os.path.join(config['output_model_path'])
 
 
 #################Function for training the model
-def train_model():
+def train_model(pandas_df, target_col, variables, model_dir):
+    """
+    Script for training logistic regression model on target_col target column
+    and predictors specified in variables list.
+    """
+    
+    X, y = pandas_df[variables].to_numpy(), pandas_df[target_col].to_numpy()
     
     #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
+    lr_model = LogisticRegression(
+        C=1.0, class_weight=None, dual=False, fit_intercept=True,
+        intercept_scaling=1, l1_ratio=None, max_iter=100,
+        multi_class='auto', n_jobs=None, penalty='l2',
+        random_state=0, solver='liblinear', tol=0.0001, verbose=0,
+        warm_start=False
+    )
     
     #fit the logistic regression to your data
+    lr_model.fit(X, y)
     
     #write the trained model to your workspace in a file called trainedmodel.pkl
+    with open(os.path.join(model_dir, 'trainedmodel.pkl'), 'wb') as model_file:
+        pickle.dump(lr_model, model_file)
 
+
+if __name__ == "__main__":
+    df = pd.read_csv(os.path.join(dataset_csv_path, 'finaldata.csv'))
+    vars = ['lastmonth_activity', 'lastyear_activity', 'number_of_employees']
+    train_model(df, 'exited', vars, model_path)
