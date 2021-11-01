865f71394dc39cce15936bcc2bccfb127e47d522
mrstelmach <marekstelmach89@gmail.com>
2021-11-01 17:18:03 +0100

    Add function for f1 scoring

--- scoring.py
+++ scoring.py
@@ -1,25 +1,37 @@
-from flask import Flask, session, jsonify, request
 import pandas as pd
-import numpy as np
 import pickle
 import os
 from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from training import get_vars_from_pandas
 import json
 
 
+def score_model(model_path, data_path, result_path):
+    """
+    Function takes a trained model, loads test data, calculates 
+    an F1 score for the model relative to the test data and writes the result 
+    to the file.
+    """
+    with open(model_path, 'rb') as model_file:
+        model = pickle.load(model_file)
+    
+    df = pd.read_csv(data_path)
+    X, y = get_vars_from_pandas(df)
+    y_pred = model.predict(X)
+    f1_score = metrics.f1_score(y, y_pred)
+    
+    with open(result_path, 'w') as result_file:
+        result_file.write(str(f1_score))
 
-#################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
-
-
-#################Function for model scoring
-def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
-
+if __name__ == "__main__":
+    with open('config.json','r') as f:
+        config = json.load(f)
+    test_data_path = os.path.join(config['test_data_path'])
+    output_model_path = os.path.join(config['output_model_path'])
+    
+    score_model(
+        os.path.join(output_model_path, 'trainedmodel.pkl'),
+        os.path.join(test_data_path, 'testdata.csv'),
+        os.path.join(output_model_path, 'latestscore.txt')
+    )
