6d51289abab6225f9f54be54a7cf19446b3214be
mrstelmach <marekstelmach89@gmail.com>
2021-11-01 23:07:49 +0100

    Add function for copying deployment files

--- deployment.py
+++ deployment.py
@@ -3,25 +3,33 @@ import pandas as pd
 import numpy as np
 import pickle
 import os
+from shutil import copyfile
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 import json
 
 
+def store_model_into_pickle(model_path, score_path, recor_path, target_dir):
+    """
+    Function copies the latest pickle file, the latestscore.txt value
+    and the ingestfiles.txt file into the target_dir directory.
+    """
+    for path in [model_path, score_path, recor_path]:
+        base_name = os.path.basename(path)
+        copyfile(path, os.path.join(target_dir, base_name))
 
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-
-
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
-
+if __name__ == "__main__":
+    with open('config.json','r') as f:
+        config = json.load(f)
+    dataset_csv_path = os.path.join(config['output_folder_path'])
+    output_model_path = os.path.join(config['output_model_path'])
+    prod_deployment_path = os.path.join(config['prod_deployment_path'])
+    
+    store_model_into_pickle(
+        os.path.join(output_model_path, 'trainedmodel.pkl'),
+        os.path.join(output_model_path, 'latestscore.txt'),
+        os.path.join(dataset_csv_path, 'ingestedfiles.txt'),
+        prod_deployment_path
+    )
