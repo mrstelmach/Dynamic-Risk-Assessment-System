7d62df0118870d86b62b359d9b795fdefb9b9894
mrstelmach <marekstelmach89@gmail.com>
2021-11-03 13:13:04 +0100

    Update with writing responses to a file

--- apicalls.py
+++ apicalls.py
@@ -1,4 +1,10 @@
+import os
+import json
 import requests
+
+with open('config.json', 'r') as f:
+    config = json.load(f)
+output_path = os.path.join(config['output_model_path'])
 
 URL = "http://127.0.0.1:8000"
 
@@ -10,10 +16,7 @@ response2 = requests.get(f'{URL}/scoring').text
 response3 = requests.get(f'{URL}/summarystats').text
 response4 = requests.get(f'{URL}/diagnostics').text
 
-#combine all API responses
-responses = #combine reponses here
+responses = f'{response1}\n{response2}\n{response3}\n{response4}'
 
-#write the responses to your workspace
-
-
-
+with open(os.path.join(output_path, 'apireturns.txt'), 'w') as api_file:
+    api_file.write(responses)
