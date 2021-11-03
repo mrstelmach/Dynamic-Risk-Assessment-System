c838a8657a1d07e9b0673547eff8ec69139eaecc
mrstelmach <marekstelmach89@gmail.com>
2021-11-03 18:24:59 +0100

    Add data ingestion and model deployment into pipeline

--- fullprocess.py
+++ fullprocess.py
@@ -1,40 +1,51 @@
 
-
+import json
+import os
+from scoring import score_model
+"""
 import training
 import scoring
 import deployment
 import diagnostics
 import reporting
+"""
 
-##################Check and read new data
-#first, read ingestedfiles.txt
-
-#second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
-
-
-
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
+with open('config.json', 'r') as f:
+    config = json.load(f)
+prod_deployment_path = os.path.join(config['prod_deployment_path'])
+input_folder_path = os.path.join(config['input_folder_path'])
 
 
-##################Checking for model drift
-#check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
+# Check for new data and if found run ingestion module.
+with open(os.path.join(prod_deployment_path, 'ingestedfiles.txt'), 'r') as f:
+    ingested_files = f.readlines()
+ingested_files = [file.rstrip() for file in ingested_files]
+current_files = [os.path.join(input_folder_path, file) 
+                 for file in os.listdir(input_folder_path)
+                 if file.endswith('.csv')]
+
+if (set(current_files) - set(ingested_files)):
+    print('Running ingestion module.')
+    os.system("python ingestion.py")
+else:
+    print('Exit.')
+    exit(0)
 
 
-##################Deciding whether to proceed, part 2
-#if you found model drift, you should proceed. otherwise, do end the process here
+# Check for model drift and if found redeploy a model.
+with open(os.path.join(prod_deployment_path, 'latestscore.txt'), 'r') as f:
+    latest_score = float(f.read())
+current_score = score_model(prod_run=True)
 
+if current_score < latest_score:
+    print('Found model drift.')
+    os.system('python training.py')
+    score_model() # get the latest score for the newly trained model
+    os.system('python deployment.py')
+else:
+    print('Exit.')
+    exit(0)
 
-
-##################Re-deployment
-#if you found evidence for model drift, re-run the deployment.py script
 
 ##################Diagnostics and reporting
 #run diagnostics.py and reporting.py for the re-deployed model
-
-
-
-
-
-
-
