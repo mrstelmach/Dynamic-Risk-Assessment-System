a41f7aa5cf9f93006845e5141f4aedffa5d9da1b
mrstelmach <marekstelmach89@gmail.com>
2021-11-03 12:08:31 +0100

    Modify scoring function

--- scoring.py
+++ scoring.py
@@ -13,13 +13,22 @@ from sklearn import metrics
 
 from training import get_vars_from_pandas
 
+with open('config.json','r') as f:
+    config = json.load(f)
+test_data_path = os.path.join(config['test_data_path'])
+output_model_path = os.path.join(config['output_model_path'])
 
-def score_model(model_path, data_path, result_path=None, target_col='exited'):
+
+def score_model(target_col='exited'):
     """
     Function takes a trained model, loads test data, calculates 
     an F1 score for the model relative to the test data and writes the result 
     to the file.
     """
+    model_path = os.path.join(output_model_path, 'trainedmodel.pkl')
+    data_path = os.path.join(test_data_path, 'testdata.csv')
+    result_path = os.path.join(output_model_path, 'latestscore.txt')
+    
     with open(model_path, 'rb') as model_file:
         model = pickle.load(model_file)
     
@@ -27,22 +36,12 @@ def score_model(model_path, data_path, result_path=None, target_col='exited'):
     X, y = get_vars_from_pandas(df, target_col)
     y_pred = model.predict(X)
     f1_score = metrics.f1_score(y, y_pred)
-    
-    if result_path is not None:
-        with open(result_path, 'w') as result_file:
-            result_file.write(str(f1_score))
+
+    with open(result_path, 'w') as result_file:
+        result_file.write(str(f1_score))
 
     return f1_score
 
 
 if __name__ == "__main__":
-    with open('config.json','r') as f:
-        config = json.load(f)
-    test_data_path = os.path.join(config['test_data_path'])
-    output_model_path = os.path.join(config['output_model_path'])
-    
-    score_model(
-        os.path.join(output_model_path, 'trainedmodel.pkl'),
-        os.path.join(test_data_path, 'testdata.csv'),
-        os.path.join(output_model_path, 'latestscore.txt')
-    )
+    score_model()
